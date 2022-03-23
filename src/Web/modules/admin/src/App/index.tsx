fcc903c04adae3dc31e73c13b3b1f08a928ff5d6
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-03-23 10:52:31 +0200

    add more method in user service

--- src/Web/modules/admin/src/App/index.tsx
+++ src/Web/modules/admin/src/App/index.tsx
@@ -1,6 +1,6 @@
+import React, { useEffect } from 'react';
 import { authService } from '@/services/auth-service';
 import { userService } from '@/services/user-service';
-import React, { useEffect } from 'react';
 
 export const App = () => {
   useEffect(() => {
@@ -9,7 +9,11 @@ export const App = () => {
       .signIn({ email: 'test@example.com', password: '11111111' })
       .then((result) => console.log(result))
       .then(() => {
-        userService.get({ page: 1 }).then((result) => console.log(result));
+        userService.get({ page: 1 }).then((result) => {
+          console.log(result);
+
+          userService.getById({ userId: result[0].id }).then((result) => console.log(result));
+        });
       });
   });
   return <div>IMBox Admin</div>;
--- src/Web/modules/admin/src/services/user-service/index.ts
+++ src/Web/modules/admin/src/services/user-service/index.ts
@@ -1,15 +1,66 @@
 import { apiClient } from '@IMBoxWeb/core/dist/api-client';
 
-const BASE_URL = '/user-service/api/users';
+interface UserModel {
+  username: string;
+  email: string;
+  password: string;
+  birthDate: Date;
+  gender: 'm' | 'f' | 'n';
+  country: string;
+  roles: [string];
+}
+
+const BASE_URL = '/user-service/api';
 
 interface GetUsersRequest {
   page: number | string;
 }
 
 const get = async (request: GetUsersRequest) => {
-  const res = await apiClient.get(`${BASE_URL}`);
+  const res = await apiClient.get(`${BASE_URL}/users`);
   const result = res.data;
   return result;
 };
 
-export const userService = Object.freeze({ get });
+interface GetUserByIdRequest {
+  userId: string;
+}
+
+const getById = async (request: GetUserByIdRequest) => {
+  const res = await apiClient.get(`${BASE_URL}/users/${request.userId}`);
+  const result = res.data;
+  return result;
+};
+
+interface CreateUserRequest {
+  user: UserModel;
+}
+
+const create = async (request: CreateUserRequest) => {
+  const res = await apiClient.post(`${BASE_URL}/users`, request.user);
+  const result = res.data;
+  return result;
+};
+
+interface UpdateUserRequest {
+  userId: string;
+  user: UserModel;
+}
+
+const update = async (request: UpdateUserRequest) => {
+  const res = await apiClient.patch(`${BASE_URL}/users/${request.userId}`, request.user);
+  const result = res.data;
+  return result;
+};
+
+interface RemoveUserRequest {
+  userId: number | string;
+}
+
+const remove = async (request: RemoveUserRequest) => {
+  const res = await apiClient.delete(`${BASE_URL}/users/${request.userId}`);
+  const result = res.data;
+  return result;
+};
+
+export const userService = Object.freeze({ get, getById, create, update, remove });
