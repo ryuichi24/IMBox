b0e3cc9d14a663af4fbf4e13b1b4e788e48cec3c
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-03-23 19:02:55 +0200

    add sidebar

--- src/Web/modules/admin/src/App/index.tsx
+++ src/Web/modules/admin/src/App/index.tsx
@@ -3,9 +3,14 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 // hooks
 import { useAuth } from '@/hooks/useAuth';
 // page components
+import { Dashboard } from '@/pages/Dashboard';
 import { SignInPage } from '@/pages/SigninPage';
 // components
 import { ProtectedRoute } from '@/components/ProtectedRoute';
+import { Sidebar } from '@/components/Layout/Sidebar';
+import { UsersPage } from '@/pages/UsersPage';
+import { MoviesPaage } from '@/pages/MoviesPage';
+import { MembersPage } from '@/pages/MembersPage';
 
 export const App = () => {
   const location = useLocation();
@@ -16,14 +21,19 @@ export const App = () => {
     <div className="container-fluid vh-100 p-0">
       <Routes>
         <Route element={<ProtectedRoute isAllowed={isAuthenticated} redirectPath="/signin" />}>
-          <Route path="/*" element={<div>Home</div>} />
+          <Route path="/" element={<Sidebar />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/movies" element={<MoviesPaage />} />
+            <Route path="/members" element={<MembersPage />} />
+          </Route>
         </Route>
 
         <Route element={<ProtectedRoute isAllowed={!isAuthenticated} redirectPath="/" />}>
           <Route path="/signin" element={<SignInPage />} />
         </Route>
 
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/dashboard" />} />
       </Routes>
     </div>
   );
---
+++ src/Web/modules/admin/src/components/Layout/Sidebar/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link, useLocation, Outlet } from 'react-router-dom';
+
+export const Sidebar = () => {
+  const { pathname } = useLocation();
+
+  const navItems = [
+    {
+      itemName: 'Dashboard',
+      path: '/dashboard',
+      icon: 'fa-solid fa-gauge',
+    },
+    {
+      itemName: 'Users',
+      path: '/users',
+      icon: 'fa-solid fa-users',
+    },
+    {
+      itemName: 'Movies',
+      path: '/movies',
+      icon: 'fa-solid fa-clapperboard',
+    },
+    {
+      itemName: 'Members',
+      path: '/members',
+      icon: 'fa-solid fa-people-group',
+    },
+  ];
+  return (
+    <div className="d-flex h-100">
+      <div className="h-100 d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{ width: '280px' }}>
+        <Link
+          to="/dashboard"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+        >
+          <span className="fs-4">IMBox Admin</span>
+        </Link>
+        <hr />
+        <ul className="nav nav-pills flex-column mb-auto">
+          {navItems.map((item, index) => (
+            <li className="nav-item" key={index}>
+              <Link to={item.path} className={`nav-link text-white ${pathname.includes(item.path) && 'active'}`}>
+                <i className={`${item.icon} pe-3`}></i>
+                {item.itemName}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <hr />
+      </div>
+      <Outlet />
+    </div>
+  );
+};
