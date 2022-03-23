11108aca1a7a2515c624f0c6fb21f2c4b0d4b640
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-03-23 21:26:28 +0200

    add header

---
+++ src/Web/modules/admin/src/components/Layout/Header/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header className="p-3 mb-3 border-bottom w-100">
+      <div className="d-flex flex-wrap flex-row-reverse align-items-center w-100">
+        <div className="dropdown text-end">
+          <a href="/" className="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown">
+            <img
+              src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+              alt="cat"
+              width="32"
+              height="32"
+              className="rounded-circle"
+            />
+          </a>
+
+          <ul className="dropdown-menu text-small">
+            <li>
+              <a className="dropdown-item" href="/">
+                Settings
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="/">
+                Profile
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className="dropdown-item" href="/">
+                Sign out
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
--- src/Web/modules/admin/src/components/Layout/index.tsx
+++ src/Web/modules/admin/src/components/Layout/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 
 export const Layout = () => {
   return (
     <div className="d-flex h-100">
       <Sidebar />
-      <div className="d-flex flex-column">
-        <div>Header</div>
+      <div className="d-flex flex-column w-100 h-100">
+        <Header />
         <Outlet />
       </div>
     </div>
